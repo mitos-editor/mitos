helix-editor/helix

default.nix: nixfmt

default.nix
@@ -6,82 +6,85 @@
   installShellFiles,
   git,
   gitRev ? null,
-  grammarOverlays ? [],
+  grammarOverlays ? [ ],
   includeGrammarIf ? _: true,
-}: let
+}:
+let
   fs = lib.fileset;
 
-  src = fs.difference (fs.gitTracked ./.) (fs.unions [
-    ./.envrc
-    ./rustfmt.toml
-    ./screenshot.png
-    ./book
-    ./docs
-    ./runtime
-    ./flake.lock
-    (fs.fileFilter (file: lib.strings.hasInfix ".git" file.name) ./.)
-    (fs.fileFilter (file: file.hasExt "svg") ./.)
-    (fs.fileFilter (file: file.hasExt "md") ./.)
-    (fs.fileFilter (file: file.hasExt "nix") ./.)
-  ]);
+  src = fs.difference (fs.gitTracked ./.) (
+    fs.unions [
+      ./.envrc
+      ./rustfmt.toml
+      ./screenshot.png
+      ./book
+      ./docs
+      ./runtime
+      ./flake.lock
+      (fs.fileFilter (file: lib.strings.hasInfix ".git" file.name) ./.)
+      (fs.fileFilter (file: file.hasExt "svg") ./.)
+      (fs.fileFilter (file: file.hasExt "md") ./.)
+      (fs.fileFilter (file: file.hasExt "nix") ./.)
+    ]
+  );
 
   # Next we actually need to build the grammars and the runtime directory
   # that they reside in. It is built by calling the derivation in the
   # grammars.nix file, then taking the runtime directory in the git repo
   # and hooking symlinks up to it.
-  grammars = callPackage ./grammars.nix {inherit grammarOverlays includeGrammarIf;};
-  runtimeDir = runCommand "helix-runtime" {} ''
+  grammars = callPackage ./grammars.nix { inherit grammarOverlays includeGrammarIf; };
+  runtimeDir = runCommand "helix-runtime" { } ''
     mkdir -p $out
     ln -s ${./runtime}/* $out
     rm -r $out/grammars
     ln -s ${grammars} $out/grammars
   '';
 in
-  rustPlatform.buildRustPackage (self: {
-    cargoLock = {
-      lockFile = ./Cargo.lock;
-      # This is not allowed in nixpkgs but is very convenient here: it allows us to
-      # avoid specifying `outputHashes` here for any git dependencies we might take
-      # on temporarily.
-      allowBuiltinFetchGit = true;
-    };
+rustPlatform.buildRustPackage (self: {
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    # This is not allowed in nixpkgs but is very convenient here: it allows us to
+    # avoid specifying `outputHashes` here for any git dependencies we might take
+    # on temporarily.
+    allowBuiltinFetchGit = true;
+  };
 
-    propagatedBuildInputs = [ runtimeDir ];
-    
-    nativeBuildInputs = [
-      installShellFiles
-      git
-    ];
+  propagatedBuildInputs = [ runtimeDir ];
 
-    buildType = "release";
+  nativeBuildInputs = [
+    installShellFiles
+    git
+  ];
 
-    name = with builtins; (fromTOML (readFile ./helix-term/Cargo.toml)).package.name;
-    src = fs.toSource {
-      root = ./.;
-      fileset = src;
-    };
+  buildType = "release";
 
-    # Helix attempts to reach out to the network and get the grammars. Nix doesn't allow this.
-    HELIX_DISABLE_AUTO_GRAMMAR_BUILD = "1";
+  name = with builtins; (fromTOML (readFile ./helix-term/Cargo.toml)).package.name;
+  src = fs.toSource {
+    root = ./.;
+    fileset = src;
+  };
 
-    # So Helix knows what rev it is.
-    HELIX_NIX_BUILD_REV = gitRev;
+  # Helix attempts to reach out to the network and get the grammars. Nix doesn't allow this.
+  HELIX_DISABLE_AUTO_GRAMMAR_BUILD = "1";
 
-    doCheck = false;
-    strictDeps = true;
+  # So Helix knows what rev it is.
+  HELIX_NIX_BUILD_REV = gitRev;
 
-    # Sets the Helix runtime dir to the grammars
-    env.HELIX_DEFAULT_RUNTIME = "${runtimeDir}";
+  doCheck = false;
+  strictDeps = true;
 
-    # Get all the application stuff in the output directory.
-    postInstall = ''
-      mkdir -p $out/lib
-      installShellCompletion ${./contrib/completion}/hx.{bash,fish,zsh}
-      mkdir -p $out/share/{applications,icons/hicolor/{256x256,scalable}/apps}
-      cp ${./contrib/Helix.desktop} $out/share/applications/Helix.desktop
-      cp ${./logo.svg} $out/share/icons/hicolor/scalable/apps/helix.svg
-      cp ${./contrib/helix.png} $out/share/icons/hicolor/256x256/apps/helix.png
-    '';
+  # Sets the Helix runtime dir to the grammars
+  env.HELIX_DEFAULT_RUNTIME = "${runtimeDir}";
 
-    meta.mainProgram = "hx";
-  })
+  # Get all the application stuff in the output directory.
+  postInstall = ''
+    mkdir -p $out/lib
+    installShellCompletion ${./contrib/completion}/hx.{bash,fish,zsh}
+    mkdir -p $out/share/{applications,icons/hicolor/{256x256,scalable}/apps}
+    cp ${./contrib/Helix.desktop} $out/share/applications/Helix.desktop
+    cp ${./logo.svg} $out/share/icons/hicolor/scalable/apps/helix.svg
+    cp ${./contrib/helix.png} $out/share/icons/hicolor/256x256/apps/helix.png
+  '';
+
+  meta.mainProgram = "hx";
+})

mitos-editor/mitos

default.nix
@@ -6,80 +6,83 @@
   installShellFiles,
   git,
   gitRev ? null,
-  grammarOverlays ? [],
+  grammarOverlays ? [ ],
   includeGrammarIf ? _: true,
-}: let
+}:
+let
   fs = lib.fileset;
 
-  src = fs.difference (fs.gitTracked ./.) (fs.unions [
-    ./.envrc
-    ./rustfmt.toml
-    ./screenshot.png
-    ./book
-    ./docs
-    ./runtime
-    ./flake.lock
-    (fs.fileFilter (file: lib.strings.hasInfix ".git" file.name) ./.)
-    (fs.fileFilter (file: file.hasExt "svg") ./.)
-    (fs.fileFilter (file: file.hasExt "md") ./.)
-    (fs.fileFilter (file: file.hasExt "nix") ./.)
-  ]);
+  src = fs.difference (fs.gitTracked ./.) (
+    fs.unions [
+      ./.envrc
+      ./rustfmt.toml
+      ./screenshot.png
+      ./book
+      ./docs
+      ./runtime
+      ./flake.lock
+      (fs.fileFilter (file: lib.strings.hasInfix ".git" file.name) ./.)
+      (fs.fileFilter (file: file.hasExt "svg") ./.)
+      (fs.fileFilter (file: file.hasExt "md") ./.)
+      (fs.fileFilter (file: file.hasExt "nix") ./.)
+    ]
+  );
 
   # Next we actually need to build the grammars and the runtime directory
   # that they reside in. It is built by calling the derivation in the
   # grammars.nix file, then taking the runtime directory in the git repo
   # and hooking symlinks up to it.
-  grammars = callPackage ./grammars.nix {inherit grammarOverlays includeGrammarIf;};
-  runtimeDir = runCommand "mitos-runtime" {} ''
+  grammars = callPackage ./grammars.nix { inherit grammarOverlays includeGrammarIf; };
+  runtimeDir = runCommand "mitos-runtime" { } ''
     mkdir -p $out
     ln -s ${./runtime}/* $out
     rm -r $out/grammars
     ln -s ${grammars} $out/grammars
   '';
 in
-  rustPlatform.buildRustPackage (self: {
-    cargoLock = {
-      lockFile = ./Cargo.lock;
-      # This is not allowed in nixpkgs but is very convenient here: it allows us to
-      # avoid specifying `outputHashes` here for any git dependencies we might take
-      # on temporarily.
-      allowBuiltinFetchGit = true;
-    };
+rustPlatform.buildRustPackage (self: {
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    # This is not allowed in nixpkgs but is very convenient here: it allows us to
+    # avoid specifying `outputHashes` here for any git dependencies we might take
+    # on temporarily.
+    allowBuiltinFetchGit = true;
+  };
 
-    propagatedBuildInputs = [ runtimeDir ];
-    
-    nativeBuildInputs = [
-      installShellFiles
-      git
-    ];
+  propagatedBuildInputs = [ runtimeDir ];
 
-    buildType = "release";
+  nativeBuildInputs = [
+    installShellFiles
+    git
+  ];
 
-    name = with builtins; (fromTOML (readFile ./crates/term/Cargo.toml)).package.name;
-    src = fs.toSource {
-      root = ./.;
-      fileset = src;
-    };
+  buildType = "release";
 
-    # Mitos attempts to reach out to the network and get the grammars. Nix doesn't allow this.
-    MITOS_DISABLE_AUTO_GRAMMAR_BUILD = "1";
+  name = with builtins; (fromTOML (readFile ./crates/term/Cargo.toml)).package.name;
+  src = fs.toSource {
+    root = ./.;
+    fileset = src;
+  };
 
-    # So Mitos knows what rev it is.
-    MITOS_NIX_BUILD_REV = gitRev;
+  # Mitos attempts to reach out to the network and get the grammars. Nix doesn't allow this.
+  MITOS_DISABLE_AUTO_GRAMMAR_BUILD = "1";
 
-    doCheck = false;
-    strictDeps = true;
+  # So Mitos knows what rev it is.
+  MITOS_NIX_BUILD_REV = gitRev;
 
-    # Sets the Mitos runtime dir to the grammars
-    env.MITOS_DEFAULT_RUNTIME = "${runtimeDir}";
+  doCheck = false;
+  strictDeps = true;
 
-    # Get all the application stuff in the output directory.
-    postInstall = ''
-      mkdir -p $out/lib
-      installShellCompletion ${./contrib/completion}/ms.{bash,fish,zsh}
-      mkdir -p $out/share/applications
-      cp ${./contrib/Mitos.desktop} $out/share/applications/Mitos.desktop
-    '';
+  # Sets the Mitos runtime dir to the grammars
+  env.MITOS_DEFAULT_RUNTIME = "${runtimeDir}";
 
-    meta.mainProgram = "ms";
-  })
+  # Get all the application stuff in the output directory.
+  postInstall = ''
+    mkdir -p $out/lib
+    installShellCompletion ${./contrib/completion}/ms.{bash,fish,zsh}
+    mkdir -p $out/share/applications
+    cp ${./contrib/Mitos.desktop} $out/share/applications/Mitos.desktop
+  '';
+
+  meta.mainProgram = "ms";
+})